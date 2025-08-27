de0d6586cf7dfb2760b50c7b787d41a6aca88566
Adithya <aadhii.yz@gmail.com>
2025-08-27 23:05:10 +0530

    Implemented InMemoryDB's Methods.

--- internal/db/in_memory.go
+++ internal/db/in_memory.go
@@ -1,28 +1,59 @@
 package db
 
-import "github.com/aadhii-yz/todo_api/internal/todo"
+import (
+	"errors"
 
-// type InMemoryDB todo.Todos
+	"github.com/aadhii-yz/todo_api/internal/todo"
+)
+
 type InMemoryDB struct {
-	data map[int]todo.Todo
+	Data   map[int]todo.Todo
+	NextID int
 }
 
 func CreateInMemoryDB() *InMemoryDB {
 	db := InMemoryDB{}
-	db.data = make(map[int]todo.Todo)
+	db.Data = make(map[int]todo.Todo)
 	return &db
 }
 
 func (db *InMemoryDB) GetAll() todo.Todos {
-	return todo.Todos{}
+	todos := todo.Todos{}
+	for _, v := range db.Data {
+		todos = append(todos, v)
+	}
+
+	return todos
 }
 
-func (db *InMemoryDB) GetById(id int) todo.Todo {
-	return todo.Todo{}
+func (db *InMemoryDB) GetById(id int) (todo.Todo, error) {
+	todo, ok := db.Data[id]
+	if !ok {
+		return todo, errors.New("Todo Not Found")
+	}
+	return todo, nil
 }
 
-func (db *InMemoryDB) Post(todo.Todo) error { return nil }
+func (db *InMemoryDB) Post(todo todo.Todo) int {
+	todo.Id = db.NextID
+	db.Data[db.NextID] = todo
+	db.NextID++
+	return db.NextID - 1
+}
 
-func (db *InMemoryDB) Patch(id int, todo todo.Todo) error { return nil }
+func (db *InMemoryDB) Patch(id int, todo todo.Todo) error {
+	if _, ok := db.Data[id]; !ok {
+		return errors.New("Todo Not Found")
+	}
+	todo.Id = id
+	db.Data[id] = todo
+	return nil
+}
 
-func (db *InMemoryDB) DeleteById(id int) error { return nil }
+func (db *InMemoryDB) DeleteById(id int) error {
+	if _, ok := db.Data[id]; !ok {
+		return errors.New("Todo Not Found")
+	}
+	delete(db.Data, id)
+	return nil
+}
--- internal/db/storage.go
+++ internal/db/storage.go
@@ -4,8 +4,8 @@ import "github.com/aadhii-yz/todo_api/internal/todo"
 
 type TodoStorage interface {
 	GetAll() todo.Todos
-	GetById(id int) todo.Todo
-	Post(todo.Todo) error
+	GetById(id int) (todo.Todo, error)
+	Post(todo todo.Todo) int
 	Patch(id int, todo todo.Todo) error
 	DeleteById(id int) error
 }
